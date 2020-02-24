1508692c604e3f89040b52c5eafa03d990e637ee
Nozomi <nozomi.kuwahara@gmail.com>
2020-02-24 18:03:57 +0900

    PitchingRecord

---
+++ PitchingRecord/RecordCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RecordCollectionViewCell.swift
+//  PitchingRecord
+//
+//  Created by 桑原望 on 2020/02/24.
+//  Copyright © 2020 MySwift. All rights reserved.
+//
+
+import UIKit
+
+class RecordCollectionViewCell: UICollectionViewCell {
+    
+}
